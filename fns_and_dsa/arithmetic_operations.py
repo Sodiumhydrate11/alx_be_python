bc95c9fa1d975f8a7a24fef3b9c5d42b9f00516e
Sodiumhydrate11 <adebayoabdulsamad960@gmail.com>
2025-07-28 17:46:42 +0100

    added arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+	"""
+	Perform a basic arithmetic operation.
+	"""
+	num1 = float(num1)
+	num2 = float(num2)
+	operation = str(operation)
+	if operation == 'add':
+		return num1 + num2
+	elif operation == 'subtract':
+		return num1 - num2
+	elif operation == 'divide':
+		if num2 == 0:
+			return "number cannot be divided by zero."
+		return num1 / num2
+	elif operation == 'multiply':
+		return num1 * num2
+	else:
+		return f"Invalid operation: {operation}"
