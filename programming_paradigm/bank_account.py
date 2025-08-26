5ba3bc810c1e5191b0300d3a3df4e76ce52a8b5a
Sodiumhydrate11 <adebayoabdulsamad960@gmail.com>
2025-08-26 16:24:28 +0100

    added main

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,23 @@
 class BankAccount:
-def __init__(self, account_balance=0):
-self.balance = account_balance
-self.transaction_history = []
+	def __init__(self, account_balance=0):
+		self.balance = account_balance
+		self.transaction_history = []
 
-def deposit(self, amount):
-if amount > 0:
-self.balance += amount
-self.transaction_history.append(f"Deposited: ${amount:.2f}")
-else:
-print("Invalid deposit amount.")
+	def deposit(self, amount):
+		if amount > 0:
+			self.balance += amount
+			self.transaction_history.append(f"Deposited: ${amount:.2f}")
+		else:
+			print("Invalid deposit amount.")
 
-def withdraw(self, amount):
-if 0 < amount <= self.balance:
-self.balance -= amount
-self.transaction_history.append(f"Withdrew: ${amount:.2f}")
-return True
-else:
-#print("Insufficient funds or invalid withdrawal amount.")
-return False
+	def withdraw(self, amount):
+		if 0 < amount <= self.balance:
+			self.balance -= amount
+			self.transaction_history.append(f"Withdrew: ${amount:.2f}")
+			return True
+		else:
+			#print("Insufficient funds or invalid withdrawal amount.")
+			return False
 
-def display_balance(self):
-print(f"Current Balance: ${self.balance:.2f}")
+	def display_balance(self):
+		print(f"Current Balance: ${self.balance:.2f}")
